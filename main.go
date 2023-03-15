2f72ea76808d77458c38ca94590fcb581c17e6a9
Kushal Beniwal <kushal.beniwal@nirmata.com>
2023-03-15 20:22:15 +0530

    show param names

--- main.go
+++ main.go
@@ -151,7 +151,7 @@ func createAWSAdapterConfigIfNotPresent(r *controllers.AWSAdapterConfigReconcile
 	}
 
 	if rp.areAllPresent() {
-		setupLog.Info("One or more of the required parameters could not be found")
+		setupLog.Info("One or more of the required parameters could not be found: clusterName='%s' clusterRegion='%s' adapterName='%s' adapterNamespace='%s'", rp.clusterName, rp.clusterRegion, rp.adapterName, rp.adapterNamespace)
 		return
 	}
 
